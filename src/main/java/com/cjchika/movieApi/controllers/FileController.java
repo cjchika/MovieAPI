91c9606ae8c6a3c70d06c8be0da2e295afc29a73
Chika Chijioke <8pluscreative@gmail.com>
2025-01-05 07:27:45 +0100

    Movie API - Files

---
+++ src/main/java/com/cjchika/movieApi/controllers/FileController.java
@@ -0,0 +1,43 @@
+package com.cjchika.movieApi.controllers;
+
+import com.cjchika.movieApi.service.FileService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+@RequestMapping("/api/file")
+public class FileController {
+    private final FileService fileService;
+
+    @Value("${project.poster}")
+
+    private String path;
+
+    public FileController(FileService fileService){
+        this.fileService = fileService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFileHandler(@RequestPart MultipartFile file) throws IOException {
+       String uploadedFileName = fileService.uploadFile(path, file);
+       return new ResponseEntity<>("File uploaded :" + uploadedFileName , HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{fileName}")
+    public void serveFileHandler(@PathVariable String fileName, HttpServletResponse response) throws IOException {
+        InputStream resourceFile = fileService.getResourceFile(path, fileName);
+        response.setContentType(MediaType.IMAGE_PNG_VALUE);
+        StreamUtils.copy(resourceFile, response.getOutputStream());
+    }
+
+}
--- src/main/java/com/cjchika/movieApi/service/FileServiceImpl.java
+++ src/main/java/com/cjchika/movieApi/service/FileServiceImpl.java
@@ -3,19 +3,33 @@ package com.cjchika.movieApi.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class FileServiceImpl implements FileService{
     @Override
     public String uploadFile(String path, MultipartFile file) throws IOException {
-        return "";
+
+        String fileName = file.getOriginalFilename();
+        String filePath = path + File.separator + fileName;
+        File f = new File(path);
+        if(!f.exists()){
+            f.mkdir();
+        }
+
+        Files.copy(file.getInputStream(), Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING);
+
+        return fileName;
     }
 
     @Override
-    public InputStream getResourceFile(String path, String name) throws FileNotFoundException {
-        return null;
+    public InputStream getResourceFile(String path, String fileName) throws FileNotFoundException {
+
+        String filePath = path + File.separator + fileName;
+
+        return new FileInputStream(filePath);
     }
 }
