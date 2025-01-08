dae4e7c7d2e7bca866abf0f4fbdf50b8db40eab7
Chika Chijioke <8pluscreative@gmail.com>
2025-01-08 21:03:40 +0100

    Movie API - Security JWT

--- src/main/java/com/cjchika/movieApi/auth/repositories/RefreshTokenRepository.java
+++ src/main/java/com/cjchika/movieApi/auth/repositories/RefreshTokenRepository.java
@@ -3,5 +3,8 @@ package com.cjchika.movieApi.auth.repositories;
 import com.cjchika.movieApi.auth.entities.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
