212bfd90bacf3999d554564564bffd0e6e2dfa04
Chika Chijioke <8pluscreative@gmail.com>
2025-01-10 00:16:15 +0100

    Movie API - Security JWT

--- src/main/java/com/cjchika/movieApi/auth/config/SecurityConfiguration.java
+++ src/main/java/com/cjchika/movieApi/auth/config/SecurityConfiguration.java
@@ -1,14 +1,12 @@
 package com.cjchika.movieApi.auth.config;
 
 import com.cjchika.movieApi.auth.services.AuthFilterService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -29,15 +27,15 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF for stateless authentication
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll() // Allow auth-related endpoints without authentication
-                        .requestMatchers("/error").permitAll()          // Allow access to /error without authentication
-                        .anyRequest().authenticated())                 // Secure all other endpoints
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/error").permitAll()
+                        .anyRequest().authenticated())
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // Stateless authentication
-                .authenticationProvider(authenticationProvider)  // Custom authentication provider
-                .addFilterBefore(authFilterService, UsernamePasswordAuthenticationFilter.class);  // Custom authentication filter
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(authFilterService, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
---
+++ src/main/java/com/cjchika/movieApi/dto/MailBody.java
@@ -0,0 +1,4 @@
+package com.cjchika.movieApi.dto;
+
+public record MailBody(String  to, String subject, String text) {
+}
